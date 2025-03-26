a5d4657e56c52577628c6ab73b9b94e0da18c260
SweetBacon1005 <nguyentrungsb1005@gmail.com>
2025-03-26 16:45:49 +0700

    update prisma schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
-import { DatabaseModule } from './database/database.module';
-import {config} from 'dotenv';
+import { PassportModule } from '@nestjs/passport';
+import { config } from 'dotenv';
+import { AuthService } from './auth/auth.service';
+import { JwtStrategy } from './auth/guard/auth.strategy';
+import { MailService } from './mail/mail.service';
 config();
-
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI as string), 
-    DatabaseModule, 
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
   ],
+  providers: [AuthService, JwtStrategy, MailService],
 })
 export class AppModule {}
